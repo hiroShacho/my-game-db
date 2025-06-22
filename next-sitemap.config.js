ba33af3eb42368f726fce72caafc9c8a63779005
hiroShacho <hiro884puni@gmail.com>
2025-06-23 02:26:27 +0900

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://tof-guide-and-db.vercel.app', // ← あなたの本番URLに合わせてください
+  siteUrl: 'https://tofguideanddb.vercel.app/', // ← あなたの本番URLに合わせてください
   generateRobotsTxt: true, // robots.txtも自動生成
   // 他にも設定があればここに追記
 }
