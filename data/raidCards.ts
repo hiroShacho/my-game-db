d973bb2afb7577d1fb057d84df46ca7f86d6a17c
hiroShacho <hiro884puni@gmail.com>
2025-10-20 07:01:08 +0900

    討伐更新

--- data/raidCards.ts
+++ data/raidCards.ts
@@ -12,11 +12,11 @@ export type RaidCard = {
 
 export const raidCards: RaidCard[] = [
   {
-    key: "ElementAlart",
-    title: "元素警戒",
-    href: "/raid/ElementAlart",
+    key: "SwarmGuard",
+    title: "ガードバグ陣",
+    href: "/raid/SwarmGuard",
     status: "開催中",
-    img: "/raid/ElementAlart.PNG",
+    img: "/raid/SwarmGuard.PNG",
     borderColor: "border-emerald-400",
     badgeColor: "bg-emerald-100",
     badgeTextColor: "text-emerald-700",
