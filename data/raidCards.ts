c58e3805aaf7c498275000a446a1149b86224195
hiroShacho <hiro884puni@gmail.com>
2026-03-30 13:32:45 +0900

    週討伐

--- data/raidCards.ts
+++ data/raidCards.ts
@@ -12,11 +12,11 @@ export type RaidCard = {
 
 export const raidCards: RaidCard[] = [
   {
-    key: "MechaSimulation",
-    title: "機兵演習",
-    href: "/raid/MechaSimulation",
+    key: "PittingPredators",
+    title: "駆虎呑狼の計",
+    href: "/raid/PittingPredators",
     status: "開催中",
-    img: "/raid/MechaSimulation.PNG",
+    img: "/raid/PittingPredators.PNG",
     borderColor: "border-emerald-400",
     badgeColor: "bg-emerald-100",
     badgeTextColor: "text-emerald-700",
